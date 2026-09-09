2fb8c0a8b335219695402ded6023611d9908ea60
Kirill Kostin <clockworkman111@gmail.com>
2026-09-09 14:46:53 +0300

    test: include local package contracts in default gate

--- tests/run.sh
+++ tests/run.sh
@@ -6,6 +6,7 @@ python3 -m unittest -v \
   "$here/test_backend.py" \
   "$here/test_backend_launcher.py" \
   "$here/test_mihomo_validation_effects.py" \
+  "$here/test_local_arch_package.py" \
   "$here/test_control_protocol.py" \
   "$here/test_control_protocol_probe.py" \
   "$here/test_control_protocol_parity.py" \
